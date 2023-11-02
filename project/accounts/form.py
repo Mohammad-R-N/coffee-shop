700f98b7d53d8f79a559f7f20315be455ea76148
mohammad-r-n <mohammad.ca@proton.me>
2023-11-02 08:30:25 -0400

    chore:USER forms

--- project/accounts/form.py
+++ project/accounts/form.py
@@ -1,39 +1,54 @@
 from typing import Any
 from django import forms
-from django.contrib.auth.models import User
+from .models import User
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={"class": "", "placeholder": "your password"}),
+    )
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput(attrs={"class": "", "placeholder": "your password"}),
+    )
 
-class UserRegistrationForm(forms.Form):
-    username=forms.CharField(min_length=5,label="Username",widget=forms.TextInput(attrs={"class":"","placeholder":"Hamed"}))
-    email=forms.EmailField(label="Email",widget=forms.EmailInput(attrs={"class":"","placeholder":"example@email.com"}))
-    password=forms.CharField(label="Password",widget=forms.PasswordInput(attrs={"class":"","placeholder":"password"}))
-    password2=forms.CharField(label="Confirm Password",widget=forms.PasswordInput(attrs={"class":"","placeholder":"password"}))
+    class Meta:
+        model = User
+        fields = (
+            "email",
+            "phone_number",
+        )
 
-    def clean_email(self):
-        email=self.cleaned_data["email"]
-        user=User.objects.filter(email=email).exists()
-        if user:
-            raise ValidationError("this email already exists")
-        return email
-    
-    def clean_username(self):
-        username=self.cleaned_data["username"]
-        user=User.objects.filter(username=username).exists()
-        if user:
-            raise ValidationError("this username already exists")
-        return username
-    
-    def clean(self) :
-        cleaned_data=super().clean()
-        p1=cleaned_data.get('password')
-        p2=cleaned_data.get('password2')
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd["password1"] and cd["password2"] and cd["password2"] != cd["password1"]:
+            raise ValidationError("PASSWORDS DO NOT MATCH")
+        return cd["password2"]
 
-        if p1 and p2 and p1 != p2:
-            raise ValidationError("password must match")
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.changed_data("password2"))
+        if commit:
+            user.save()
+        return user
 
 
-class UserLoginForm(forms.Form):
-    username=forms.CharField(min_length=5,label="Username",widget=forms.TextInput(attrs={"class":"","placeholder":"Hamed"}))
-    password=forms.CharField(label="Password",widget=forms.PasswordInput(attrs={"class":"","placeholder":"password"}))
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(
+        help_text="You can change password from this form <a href='../password/'>THIS FORM</a>"
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "email",
+            "phone_number",
+            "password",
+            "last_login",
+            "first_name",
+            "last_name",
+            "age",
+        )
