c688703b89162eda36798c30e3d1429a0ec0a003
mohammad-r-n <mohammad.ca@proton.me>
2023-11-02 08:31:22 -0400

    chore: basics

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
 from . import views
 
-app_name="accounts"
-urlpatterns=[
-    path('registration/',views.UserRegisterView.as_view(),name="register"),
-    path('login/',views.UserLoginView.as_view(),name="login"),
-    path('logout/',views.UserLogoutView.as_view(),name="logout"),
-] 
+app_name = "accounts"
+urlpatterns = []
