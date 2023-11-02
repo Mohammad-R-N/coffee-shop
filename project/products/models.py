dc27e842d2794ec06e53cb5567cbbd643f9a7e39
mohammad-r-n <mohammad.ca@proton.me>
2023-11-02 19:26:24 -0400

    chore: Product model added

--- project/products/models.py
+++ project/products/models.py
@@ -21,3 +21,29 @@ class Category(models.Model):
                 self.slug,
             ],
         )
+
+
+class Product(models.Model):
+    category = models.ManyToManyField(Category, related_name="products")
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to="products/%Y/%m/%d/")
+    description = models.TextField()
+    price = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse(
+            "core:product_detail",
+            args=[
+                self.slug,
+            ],
+        )
