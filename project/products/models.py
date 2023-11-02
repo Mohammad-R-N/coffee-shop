dbb2960f1610791df7531cae77decbd0a36aaab6
mohammad-r-n <mohammad.ca@proton.me>
2023-11-02 19:26:00 -0400

    chore: Category model added

--- project/products/models.py
+++ project/products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse(
+            "core:category_filter",
+            args=[
+                self.slug,
+            ],
+        )
