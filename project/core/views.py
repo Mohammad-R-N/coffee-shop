6dc70ace1e03563ed2c5417631e6ef81d230b40c
mohammad-r-n <mohammad.ca@proton.me>
2023-11-02 19:25:28 -0400

    chore: basic home view

--- project/core/views.py
+++ project/core/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from products.models import Category, Product
 
 
 class HomeView(View):
-    
-    def get(self,request):
-
-        return render(request,'core/home-page.html')
+    def get(self, request):
+        products = Product.objects.filter(available=True)
+        return render(request, "core/home-page.html", {"products": products})
