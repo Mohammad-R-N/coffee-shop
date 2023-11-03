d8070c4916a22c043bddb7904c29c2ab19a14861
mohammad-r-n <mohammad.ca@proton.me>
2023-11-03 19:53:19 -0400

    chore: add carts urls in config

--- project/config/urls.py
+++ project/config/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("", include("core.urls", namespace="core")),
     path("accounts/", include("accounts.urls", namespace="accounts")),
     path("products/", include("products.urls", namespace="products")),
+    path("carts/", include("carts.urls", namespace="carts")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
