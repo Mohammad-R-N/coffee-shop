cce15596bb2d93b2f206bd8d16c0167a3ab9cdfb
mohammad-r-n <mohammad.ca@proton.me>
2023-11-03 19:46:08 -0400

    chore: product detail View added

--- project/products/views.py
+++ project/products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+from .models import Product
+from . import tasks
+from utils import IsAdminUserMixin
+from django.contrib import messages
 
-# Create your views here.
+
+class ProductDetailView(View):
+    def get(self, request, slug):
+        product = get_object_or_404(Product, slug=slug)
+        return render(request, "products/product-detail.html", {"product": product})
+
+    def post(self, request):
+        pass
