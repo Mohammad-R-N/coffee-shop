9c1b3155b7351d01134644ae2541e521ea8b3f68
mohammad-r-n <mohammad.ca@proton.me>
2023-11-03 19:34:17 -0400

    chore: added login and logout urls

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -5,4 +5,6 @@ app_name = "accounts"
 urlpatterns = [
     path("registration/", views.UserRegistrationView.as_view(), name="registration"),
     path("otp/", views.OtpView.as_view(), name="otp"),
+    path("login/", views.UserLoginView.as_view(), name="user_login"),
+    path("logout/", views.UserLogoutView.as_view(), name="user_logout"),
 ]
