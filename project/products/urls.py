396fd63a25dda092c9ff10527a6ad2ae1e781011
mohammad-r-n <mohammad.ca@proton.me>
2023-11-03 19:42:19 -0400

    chore: Bucket urls added

--- project/products/urls.py
+++ project/products/urls.py
@@ -1,6 +1,24 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-app_name="products"
-urlpatterns=[
-] 
+app_name = "products"
+
+bucket_urls = [
+    path("", views.BucketHome.as_view(), name="bucket"),
+    path(
+        "delete_obj/<str:key>/",
+        views.DeleteBucketObject.as_view(),
+        name="delete_obj_bucket",
+    ),
+    path(
+        "download_obj/<str:key>/",
+        views.DownloadBucketObject.as_view(),
+        name="download_obj_bucket",
+    ),
+]
+
+
+urlpatterns = [
+    path("bucket/", include(bucket_urls)),
+    path("<slug:slug>/", views.ProductDetailView.as_view(), name="product-detail"),
+]
