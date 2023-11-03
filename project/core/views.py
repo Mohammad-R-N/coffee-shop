b392efe0b650fd0f667b3a782c2a4e1f423835d3
mohammad-r-n <mohammad.ca@proton.me>
2023-11-03 19:39:59 -0400

    chore: Home view created

--- project/core/views.py
+++ project/core/views.py
@@ -4,6 +4,14 @@ from products.models import Category, Product
 
 
 class HomeView(View):
-    def get(self, request):
+    def get(self, request, category_slug=None):
         products = Product.objects.filter(available=True)
-        return render(request, "core/home-page.html", {"products": products})
+        categories = Category.objects.filter(is_sub=False)
+        if category_slug:
+            category = Category.objects.get(slug=category_slug)
+            products = Product.objects.filter(category=category)
+        return render(
+            request,
+            "core/home-page.html",
+            {"products": products, "categories": categories},
+        )
