3b8417bcb57bc909c6def8c16e439a42058b29e2
mohammad-r-n <mohammad.ca@proton.me>
2023-11-03 19:39:44 -0400

    chore: category url added

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name="core"
-urlpatterns=[
-    path('',views.HomeView.as_view(),name="home-page")
-] 
+app_name = "core"
+urlpatterns = [
+    path("", views.HomeView.as_view(), name="home-page"),
+    path(
+        "category/<slug:category_slug>/", views.HomeView.as_view(), name="category-slug"
+    ),
+]
