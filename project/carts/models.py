b30ef1a55acb0b0f6f013c5159d8279888365c05
mohammad-r-n <mohammad.ca@proton.me>
2023-11-05 00:47:33 -0400

    chore: Order model

--- project/carts/models.py
+++ project/carts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from accounts.models import User
+from products.models import Product
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    paid = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    discount = models.IntegerField(blank=True, null=True, default=None)
+
+    class Meta:
+        ordering = ("paid", "-updated")
+
+    def __str__(self):
+        return f"{self.user} - {str(self.id)}"
+
+    def get_total_price(self):
+        total = sum(item.get_cost() for item in self.items.all())
+        if self.discount:
+            discount_price = (self.discount / 100) * total
+            return int(total - discount_price)
+        return total
