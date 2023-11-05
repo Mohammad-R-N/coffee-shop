918cd3759fb97a646420e9c0985103700ca3b180
mohammad-r-n <mohammad.ca@proton.me>
2023-11-05 11:19:09 -0500

    chore: Comment model registration

--- project/interactions/admin.py
+++ project/interactions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "product", "created", "is_reply")
+    raw_id_fields = ("user", "product", "reply")
