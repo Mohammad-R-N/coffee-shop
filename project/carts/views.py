5fe121fbbf70067e7a08ae1cb93b1692a3041f52
mohammad-r-n <mohammad.ca@proton.me>
2023-11-05 00:49:53 -0400

    chore: CartRemove View

--- project/carts/views.py
+++ project/carts/views.py
@@ -24,3 +24,11 @@ class CartAddView(View):
         if form.is_valid():
             cart.add(product, form.cleaned_data["quantity"])
         return redirect("carts:cart")
+
+
+class CartRemoveView(View):
+    def get(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        cart.remove(product)
+        return redirect("carts:cart")
