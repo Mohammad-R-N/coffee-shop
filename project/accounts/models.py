5e047653ddb9c0e29e309490139432d04bf2df45
mohammad-r-n <mohammad.ca@proton.me>
2023-11-05 00:41:56 -0400

    chore: update User model

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from .manager import UserManager
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     phone_number = models.CharField(max_length=11, unique=True)
     email = models.EmailField(max_length=200, unique=True)
     first_name = models.CharField(max_length=100, blank=True, null=True)
@@ -19,12 +19,6 @@ class User(AbstractBaseUser):
 
     def __str__(self) -> str:
         return f"{self.phone_number} - {self.email}"
-
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
 
     @property
     def is_staff(self):
