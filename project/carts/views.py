d7a41d9d3cefdbe85b19d0fc0076856966d788dd
mohammad-r-n <mohammad.ca@proton.me>
2023-11-05 00:48:53 -0400

    chore: Cart View

--- project/carts/views.py
+++ project/carts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from utils import Cart
+from .forms import CartAddForm, CouponApplyForm
+from products.models import Product
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Order, OrderItem, Coupon
+import datetime
+from django.contrib import messages
 
-# Create your views here.
+
+class CartView(View):
+    def get(self, request):
+        cart = Cart(request)
+        return render(request, "carts/cart.html", {"cart": cart})
