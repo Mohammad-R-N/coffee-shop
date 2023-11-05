c94e3809650ea97fc73ec773386eced02735f86d
mohammad-r-n <mohammad.ca@proton.me>
2023-11-05 00:49:27 -0400

    chore: CartAdd View

--- project/carts/views.py
+++ project/carts/views.py
@@ -14,3 +14,13 @@ class CartView(View):
     def get(self, request):
         cart = Cart(request)
         return render(request, "carts/cart.html", {"cart": cart})
+
+
+class CartAddView(View):
+    def post(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        form = CartAddForm(request.POST)
+        if form.is_valid():
+            cart.add(product, form.cleaned_data["quantity"])
+        return redirect("carts:cart")
