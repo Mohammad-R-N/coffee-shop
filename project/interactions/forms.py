862b8d678b931fb8263e57c9bbbeb14b9c712050
mohammad-r-n <mohammad.ca@proton.me>
2023-11-06 16:45:21 -0500

    chore: forms updated

--- project/interactions/forms.py
+++ project/interactions/forms.py
@@ -6,10 +6,27 @@ class CommentCreateForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ("body",)
-        widgets = {"body": forms.Textarea(attrs={"class": "form-control"})}
+        widgets = {
+            "body": forms.Textarea(
+                attrs={
+                    "class": "form-control bg-light text-dark",
+                    "style": "height: 170px; width: 60%;",
+                    "placeholder": "NEW COMMENT...",
+                }
+            )
+        }
 
 
 class CommentReplyForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ("body",)
+        widgets = {
+            "body": forms.Textarea(
+                attrs={
+                    "class": "form-control bg-light",
+                    "style": "height: 100px; width: 40%;",
+                    "placeholder": "ANSWER...",
+                }
+            )
+        }
