85c55bf63e1d5a0841bd3dda5cb143697bf392d8
mohammad-r-n <mohammad.ca@proton.me>
2023-11-06 16:44:48 -0500

    chore: forms updated

--- project/carts/forms.py
+++ project/carts/forms.py
@@ -2,8 +2,14 @@ from django import forms
 
 
 class CartAddForm(forms.Form):
-    quantity = forms.IntegerField(min_value=1, max_value=9)
+    quantity = forms.IntegerField(
+        min_value=1,
+        max_value=9,
+        widget=forms.NumberInput(attrs={"class": "btn btn-warning", "value": "1"}),
+    )
 
 
 class CouponApplyForm(forms.Form):
-    code = forms.CharField()
+    code = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "btn btn-warning text-dark"})
+    )
