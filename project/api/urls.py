9b9bbc3acc1e07edb46c319243930f14b0e97e1f
mohammad-r-n <mohammad.ca@proton.me>
2023-11-09 01:42:10 -0500

    chore: user info api url

--- project/api/urls.py
+++ project/api/urls.py
@@ -6,4 +6,7 @@ from .views import carts_views as cv
 
 
 app_name = "api"
-urlpatterns = [path("register/", av.UserRegister.as_view(), name="register-api")]
+urlpatterns = [
+    path("register/", av.UserRegister.as_view(), name="register-api"),
+    path("userinfo/", av.UserInfo.as_view(), name="userinfo-api"),
+]
